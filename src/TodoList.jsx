2964a1e6ed1fd4222e037aefa4ba3dffe00db655
satyam shukla <satyamshukla9528@gmail.com>
2026-04-07 12:54:16 +0530

    update todo functions

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,15 +1,22 @@
 import {useState } from "react";
+import {v4 as uuidv4 } from 'uuid';
 
 export default function TodoList(){
-    let [todos, setTodos] = useState(["sample task"]);
+    let [todos, setTodos] = useState([{task: "sample-task", id:uuidv4()}]);
     let [NewTodo, setNewTodo] = useState("");
     let addNewTask =()=>{
-        setTodos([...todos, NewTodo]);
+        setTodos((prevTodos)=>{
+            return [...prevTodos, {task: NewTodo, id: uuidv4()}]
+        });
         setNewTodo("");
     };
     let updateTodoValue = (event) => {
         setNewTodo(event.target.value);
-    }
+    };
+
+    let deleteTodo =(id)=>{
+      setTodos((prevTodos) => todos.filter((prevTodos)=> prevTodos.id !=id));
+    };
 
 
     return (
@@ -24,7 +31,12 @@ export default function TodoList(){
             <h4>Task Todo</h4>
             <ul> 
                 {todos.map((todo)=>(
-                    <li>{todo}</li> 
+                    <li key={todo.id}>
+                        <span>{todo.task}</span>
+                        &nbsp; &nbsp; &nbsp;
+                        <button onClick={()=>deleteTodo(todo.id)}>delete</button>
+                        </li> 
+
                 ))}
             </ul>
         </div>
